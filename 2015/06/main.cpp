df1800724c4df9b12c53a0010e2a1648339c0a7b
Adam Ellis <adelli@microsoft.com>
2019-06-20 16:48:54 -0700

    Completed 2015.6

--- 2015/06/main.cpp
+++ 2015/06/main.cpp
@@ -13,14 +13,19 @@ enum Action
 const size_t FIELD_WIDTH = 1000;
 const size_t FIELD_HEIGHT = 1000;
 
-void ChangeLights(bool lights[FIELD_WIDTH][FIELD_HEIGHT], int x1, int y1, int x2, int y2, Action action);
+void ChangeLights(bool* lights, int x1, int y1, int x2, int y2, Action action);
+
+void TurnOffLights(int* lights);
+void ChangeLights(int* lights, int x1, int y1, int x2, int y2, Action action);
 
 int main(int argc, char* argv)
 {
 	std::string line;
-	char command[10];
-	bool lights[FIELD_WIDTH][FIELD_HEIGHT];
+	bool* lights = new bool[FIELD_WIDTH * FIELD_HEIGHT];
 	ChangeLights(lights, 0, 0, FIELD_WIDTH - 1, FIELD_HEIGHT - 1, TurnOff);
+
+	int* lightsWithBrightness = new int [FIELD_WIDTH * FIELD_HEIGHT];
+	TurnOffLights(lightsWithBrightness);
 
 	std::ifstream file("input06.txt");
 	while (getline(file, line))
@@ -46,36 +51,49 @@ int main(int argc, char* argv)
 		if (token == "on")
 		{
 			ChangeLights(lights, x1, y1, x2, y2, TurnOn);
+			ChangeLights(lightsWithBrightness, x1, y1, x2, y2, TurnOn);
 		}
 		if (token == "off")
 		{
 			ChangeLights(lights, x1, y1, x2, y2, TurnOff);
+			ChangeLights(lightsWithBrightness, x1, y1, x2, y2, TurnOff);
 		}
 		if (token == "toggle")
 		{
 			ChangeLights(lights, x1, y1, x2, y2, Toggle);
+			ChangeLights(lightsWithBrightness, x1, y1, x2, y2, Toggle);
 		}
 	}
 	file.close();
 
 	int lightsCount = 0;
+	int totalBrightness = 0;
 	for (int x = 0; x < FIELD_WIDTH; ++x)
 	{
 		for (int y = 0; y < FIELD_HEIGHT; ++y)
 		{
-			if (lights[x][y])
+			if (lights[x * FIELD_HEIGHT + y])
 			{
 				++lightsCount;
 			}
+
+			totalBrightness += lightsWithBrightness[x * FIELD_HEIGHT + y];
 		}
 	}
 
-	std::cout << lightsCount << " lights are on." << std::endl;
+	std::cout << lightsCount << " lights are on with the old plan." << std::endl;
+	std::cout << "The total brightness from the new directions is " << totalBrightness << "." << std::endl;
+
+	delete [] lights;
+	lights = NULL;
+
+	delete[] lightsWithBrightness;
+	lightsWithBrightness = NULL;
 
 	return 0;
 }
 
-void ChangeLights(bool lights[FIELD_WIDTH][FIELD_HEIGHT], int x1, int y1, int x2, int y2, Action action)
+void ChangeLights(bool* lights, int x1, int y1, int x2, int y2, Action action)
 {
 	for (int x = x1; x <= x2; ++x)
 	{
@@ -84,13 +102,49 @@ void ChangeLights(bool lights[FIELD_WIDTH][FIELD_HEIGHT], int x1, int y1, int x2
 			switch (action)
 			{
 			case TurnOn:
-				lights[x][y] = true;
+				lights[x * FIELD_HEIGHT + y] = true;
 				break;
 			case TurnOff:
-				lights[x][y] = false;
+				lights[x * FIELD_HEIGHT + y] = false;
 				break;
 			case Toggle:
-				lights[x][y] = !lights[x][y];
+				lights[x * FIELD_HEIGHT + y] = !lights[x * FIELD_HEIGHT + y];
+				break;
+			}
+		}
+	}
+}
+
+void TurnOffLights(int* lights)
+{
+	for (int x = 0; x < FIELD_WIDTH; ++x)
+	{
+		for (int y = 0; y < FIELD_HEIGHT; ++y)
+		{
+			lights[x * FIELD_HEIGHT + y] = 0;
+		}
+	}
+}
+
+void ChangeLights(int* lights, int x1, int y1, int x2, int y2, Action action)
+{
+	for (int x = x1; x <= x2; ++x)
+	{
+		for (int y = y1; y <= y2; ++y)
+		{
+			switch (action)
+			{
+			case TurnOn:
+				lights[x * FIELD_HEIGHT + y] += 1;
+				break;
+			case TurnOff:
+				if (lights[x * FIELD_HEIGHT + y] > 0)
+				{
+					lights[x * FIELD_HEIGHT + y] -= 1;
+				}
+				break;
+			case Toggle:
+				lights[x * FIELD_HEIGHT + y] += 2;
 				break;
 			}
 		}
