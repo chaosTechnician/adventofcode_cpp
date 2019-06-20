78e75ca9831cae75eb01f38c7ae8543385a16772
Adam Ellis <adelli@microsoft.com>
2019-06-20 13:05:37 -0700

    Completed 2015.4

--- 2015/04/main.cpp
+++ 2015/04/main.cpp
@@ -2,9 +2,20 @@
 #include <string>
 #include "md5.h"
 
+size_t FindNumber(const char* secretKey, size_t numberOfZeroes);
+
 int main(int argc, char* argv)
 {
 	const char* secretKey = "bgvyzdsv";
+
+	std::cout << "Number for 5 zeroes: " << FindNumber(secretKey, 5) << std::endl;
+	std::cout << "Number for 6 zeroes: " << FindNumber(secretKey, 6) << std::endl;
+
+	return 0;
+}
+
+size_t FindNumber(const char* secretKey, size_t numberOfZeroes)
+{
 	const size_t valueSize = 50;
 	size_t number = 0;
 	bool foundIt = false;
@@ -15,7 +26,7 @@ int main(int argc, char* argv)
 		sprintf_s(value, valueSize, "%s%zi", secretKey, number);
 		std::string hash = md5(std::string(value));
 		foundIt = true;
-		for (int i = 0; i < 5 && foundIt; ++i)
+		for (int i = 0; i < numberOfZeroes && foundIt; ++i)
 		{
 			foundIt = (hash[i] == '0');
 		}
@@ -26,7 +37,5 @@ int main(int argc, char* argv)
 		}
 	} while (!foundIt);
 
-	std::cout << "Number = " << number;
-
-	return 0;
+	return number;
 }
